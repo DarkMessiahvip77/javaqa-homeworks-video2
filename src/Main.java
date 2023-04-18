de7a5d01cd36096f80690a721a0c8607c13a98d5
“Ilya” <mitsukiv@yandex.ru>
2023-04-18 21:09:46 +0300

    add .gitignore

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Супер бонус");
+        int ticket = 11_000; // цена билета//
+        int result;
+        result = ticket / 20;
+        System.out.println(result);
+        }
+}
